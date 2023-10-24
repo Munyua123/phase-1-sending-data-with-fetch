330ff716f633ce69f038a87621e084d3d8cd7d21
Earl <earlmunyua96@gmail.com>
2023-10-24 13:17:41 +0300

    first

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name, email){
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers:{
+            "content-type": "application/json",
+            "Accept" : "application/json",
+        }, 
+        body: JSON.stringify({
+            name : name,
+            email : email,
+        }),
+    })
+        .then(res => (res.json()))
+        .then(users => console.log(users))
+        .catch((error) => {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+        })
+        
+}
+
+submitData("Steve", "steve@steve.com")
